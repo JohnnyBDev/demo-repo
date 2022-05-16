0279353db5c75f23ed9b3fe115caf8410522e7b0
ntcEldan <ntc.eldan@gmail.com>
2022-05-16 13:32:49 +0300

    Revert "did it worked?"
    This reverts commit 47d909e0fb4a8baf65e6899ea437cc8ffb013f6f.

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
 	<h1>Hello world! </h1>
 <h2>hello test</h2>
 
+<h2>hello test</h2>
+
 
 <h1>Testing Revert</h1>
 
 <p>lets try different changes</p>
 
 
-<p>lets try again to change it</p>
+
+
 
 <h1>Testing Revert!</h1>
-
 
