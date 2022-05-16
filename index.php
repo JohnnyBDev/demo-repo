abaec3a8e01e2d221abc6ebb0cbde5c4ae0307c3
ntcEldan <ntc.eldan@gmail.com>
2022-05-16 12:34:20 +0300

    Revert "Revert "trying to revert things""
    This reverts commit d287120be2a87762046a380e115c5aea465c2185.
    
    :wq

--- index.php
+++ index.php
@@ -9,9 +9,13 @@
 
 <h2>hello test</h2>
 
+<<<<<<< HEAD
 <h1>Testing Revert</h1>
 
 <p>lets try different changes</p>
 
 
 
+=======
+<h1>Testing Revert!</h1>
+>>>>>>> parent of d287120 (Revert "trying to revert things")
