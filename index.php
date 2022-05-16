47d909e0fb4a8baf65e6899ea437cc8ffb013f6f
ntcEldan <ntc.eldan@gmail.com>
2022-05-16 12:36:08 +0300

    did it worked?

--- index.php
+++ index.php
@@ -7,15 +7,14 @@
 	<h1>Hello world! </h1>
 <h2>hello test</h2>
 
-<h2>hello test</h2>
 
-<<<<<<< HEAD
 <h1>Testing Revert</h1>
 
 <p>lets try different changes</p>
 
 
 
-=======
+
 <h1>Testing Revert!</h1>
->>>>>>> parent of d287120 (Revert "trying to revert things")
+
+
